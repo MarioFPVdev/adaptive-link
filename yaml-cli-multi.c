8597070cfe5caeedb89ef2d98e9add98d26e4caf
MarioFPV <57532232+MarioFPVdev@users.noreply.github.com>
2025-07-03 09:09:25 +0100

    Update yaml-cli-multi.c

--- yaml-cli-multi.c
+++ yaml-cli-multi.c
@@ -1,5 +1,4 @@
 /*
- * Original source code by Joakim(https://github.com/snokvist) : https://github.com/sickgreg/OpenIPC-air_manager/blob/main/src/stupid-yaml.c
  * YAML Configurator
  *
  * This program parses a subset of YAML configuration files and supports:
